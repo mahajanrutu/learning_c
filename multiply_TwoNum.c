699a6e75ba2748c03864c52919182cdc5ca2fb1e
mahajanrutu <156782347+mahajanrutu@users.noreply.github.com>
2024-02-29 18:43:41 +0000

    Multiplication of two number

---
+++ multiply_TwoNum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    int a,b,mul;
+
+    printf("Enter a number:");
+    scanf("%d%d", &a,&b);
+
+    mul=a*b;
+
+    printf("Multiplcation of two numbers is:%d", mul);
+
+    return 0;
+
+}
